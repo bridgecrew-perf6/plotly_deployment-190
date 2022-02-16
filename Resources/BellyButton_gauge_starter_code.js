9236c731a50cb2ad3b78638af85da2e597a07303
kaalrok <92065902+kaalrok@users.noreply.github.com>
2022-02-16 16:16:42 +0100

    updating part of Gauge

--- Resources/BellyButton_gauge_starter_code.js
+++ Resources/BellyButton_gauge_starter_code.js
@@ -6,9 +6,12 @@ function buildCharts(sample) {
 
     // Create a variable that holds the samples array. 
 
+
     // Create a variable that filters the samples for the object with the desired sample number.
 
+
     // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    
 
     // Create a variable that holds the first sample in the array.
   
--- Static/js/charts.js
+++ Static/js/charts.js
@@ -4,7 +4,7 @@ function init() {
 
   // Use the list of sample names to populate the select options
   d3.json("samples.json").then((data) => {
-    console.log(data);
+    
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -54,24 +54,32 @@ function buildMetadata(sample) {
   });
 }
 
-// 1. Create the buildCharts function.
+// Deliverable 1, Step 1. Create the buildCharts function.
 function buildCharts(sample) {
-  // 2. Use d3.json to load and retrieve the samples.json file 
+  // Deliverable 1, Step 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
 
-    // 3. Create a variable that holds the samples array. 
+    // Deliverable 1, Step 3. Create a variable that holds the samples array. 
     var samples = data.samples;
     console.log(samples);
 
-    // 4. Create a variable that filters the samples for the object with the desired sample number.
+    // Deliverable 1, Step 4. Create a variable that filters the samples for the object with the desired sample number.
     var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
 
+    // Delivierable 3, Step 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metadata = meta.metadata;
 
-    //  5. Create a variable that holds the first sample in the array.
+      // Create a variable that holds the first sample in the array.
+    var metadataResultArray = metadata.filter(sampleObj => sampleObj.id ==sample);
+
+    // Deliverable 1, Step 5. Create a variable that holds the first sample in the array.
     var result = resultArray[0];
     console.log(result);
 
-    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    // Delivierable 3, Step 2. Create a variable that holds the first sample in the metadata array.
+    var metadataResult = metadataResultArray[0];
+
+    // Deliverable 1, Step 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     // otu_ids
     var otu_ids = result.otu_ids
     console.log(otu_ids)
@@ -83,6 +91,9 @@ function buildCharts(sample) {
     // sample_values
     var sample_values = result.sample_values
     console.log(sample_values)
+
+    // Delivierable 3, Step 3. Create a variable that holds the washing frequency.
+    var washFreq = parseFloat(metadataResult.wfreq);
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
@@ -137,6 +148,20 @@ function buildCharts(sample) {
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+    // Start of Gauge
+    // Delivierable 3, Step 4. Create the trace for the gauge chart.
+    var gaugeData = [
+     
+    ];
+    
+    // Delivierable 3, Step 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+     
+    };
+
+    // Delivierable 3, Step 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot();
   });
 
   // samples.json no longer available
